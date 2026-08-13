6b21a8ce084875fe69cee63e379c8d1609d16568
Zawad-droid <140142191+Zawad-droid@users.noreply.github.com>
2026-08-12 22:10:22 -0600

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<int, vector<int>>> pq;
+        for(int i = 0; i < points.size(); i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            int dis = (x * x) + (y * y);
+            pq.push({dis, {x, y}});
+            if(pq.size() > k) pq.pop();
+        }
+        vector<vector<int>> res;
+        while(!pq.empty()){
+            res.push_back({pq.top().second[0], pq.top().second[1]});
+            pq.pop();
+        }
+        return res;
+    }
+};
